e5c96a0c6688ab472c10d177caf4b80830a9241c
Bruna <brunaporato@gmail.com>
2023-11-10 15:41:12 -0300

    fix routes for deploy

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,10 +7,10 @@ import { Success } from "./pages/Success";
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<DefaultLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/success" element={<Success />} />
+      <Route path="/cofee-delivery" element={<DefaultLayout />}>
+        <Route path="/coffee-delivery" element={<Home />} />
+        <Route path="/coffee-delivery/checkout" element={<Checkout />} />
+        <Route path="/coffee-delivery/success" element={<Success />} />
       </Route>
     </Routes>
   )
